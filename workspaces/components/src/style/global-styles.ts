7b4acc647590014b174fd0bbca53de5cf666286d
Rob Walsh <r.walsh.21@gmail.com>
2023-05-21 10:01:52 +0100

    styles, theme wip

---
+++ workspaces/components/src/style/global-styles.ts
@@ -0,0 +1,16 @@
+export const styles = {
+  global: {
+    html: {
+      fontSize: '16px',
+      scrollPaddingTop: '140px',
+      padding: 0,
+      margin: 0,
+    },
+    body: {
+      background: 'bg-default',
+      color: 'fg-default',
+      padding: 0,
+      margin: 0,
+    },
+  },
+};
