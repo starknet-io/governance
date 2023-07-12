79cb48f4177b5cca31448c972aee54e06216a37c
Rob Walsh <r.walsh.21@gmail.com>
2023-07-12 15:06:25 +0100

    add navgroup action

--- workspaces/components/src/Navigation/NavGroup/NavGroup.tsx
+++ workspaces/components/src/Navigation/NavGroup/NavGroup.tsx
@@ -1,18 +1,19 @@
-import { Flex, Stack, Text } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface NavGroupProps {
   label?: string;
   children: ReactNode;
   alignEnd?: boolean;
+  action?: ReactNode;
 }
 
 export const NavGroup = (props: NavGroupProps) => {
-  const { label, children, alignEnd } = props;
+  const { label, action, children, alignEnd } = props;
   return (
     <Flex
       flexDirection="column"
-      marginTop={alignEnd ? 'auto' : '24px'}
+      marginTop={alignEnd ? "auto" : "24px"}
       padding="0"
     >
       {label && (
@@ -24,8 +25,14 @@ export const NavGroup = (props: NavGroupProps) => {
           letterSpacing="widest"
           color="gray.500"
           mb="3"
+          position="relative"
         >
           {label}
+          {action && (
+            <Box position="absolute" right="0" top="-12px">
+              {action}
+            </Box>
+          )}
         </Text>
       )}
       <Stack spacing="1">{children}</Stack>
