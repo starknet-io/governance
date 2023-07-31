a4fe684c5a5742886153c3523f067e1b2d9ea5dd
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-07-31 15:56:49 +0400

    fix VITE_APP_SNAPSHOT_SPACE stringToHex

--- workspaces/frontend/src/pages/delegates/profile/@id.page.tsx
+++ workspaces/frontend/src/pages/delegates/profile/@id.page.tsx
@@ -67,7 +67,7 @@ export function Page() {
 
   const delegation = useDelegateRegistryDelegation({
     address: import.meta.env.VITE_APP_DELEGATION_REGISTRY,
-    args: [address!, stringToHex(import.meta.env.VITE_APP_SNAPSHOT_SPACE)],
+    args: [address!, stringToHex(import.meta.env.VITE_APP_SNAPSHOT_SPACE, { size: 32 })],
     watch: true,
     chainId: parseInt(import.meta.env.VITE_APP_DELEGATION_CHAIN_ID),
     enabled: address != null,
@@ -129,7 +129,7 @@ export function Page() {
         delegateTokens={() => {
           write?.({
             args: [
-              stringToHex(import.meta.env.VITE_APP_SNAPSHOT_SPACE),
+              stringToHex(import.meta.env.VITE_APP_SNAPSHOT_SPACE, { size: 32 }),
               delegate?.author?.address as any,
             ],
           });
