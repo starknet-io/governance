789ae0fd4ed493ca67dc2550f3214a031e8eb492
Rob Walsh <r.walsh.21@gmail.com>
2023-09-03 13:13:44 +0100

    add banner to home

--- workspaces/frontend/src/pages/index.page.tsx
+++ workspaces/frontend/src/pages/index.page.tsx
@@ -1,13 +1,9 @@
 import { DocumentProps } from "src/renderer/types";
 
-import { Box, PageTitle } from "@yukilabs/governance-components";
+import { BannerHome, Box, PageTitle } from "@yukilabs/governance-components";
 
 export function Page() {
-  return (
-    <Box px={{ base: "26.5px", md: "76.5px" }} pt="40px" pb="200px">
-      <PageTitle title="Home" />
-    </Box>
-  );
+  return <BannerHome snipCount={1} />;
 }
 
 export const documentProps = {
