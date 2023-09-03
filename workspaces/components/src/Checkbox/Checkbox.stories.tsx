c0833e3067692dbb4ead216fc994ce6d276369fc
Rob Walsh <r.walsh.21@gmail.com>
2023-09-03 17:34:33 +0100

    update checkbox stories

--- workspaces/components/src/Checkbox/Checkbox.stories.tsx
+++ workspaces/components/src/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import { VStack, Checkbox as ChakraCheckbox } from "@chakra-ui/react";
+import { VStack, Checkbox as ChakraCheckbox, Box } from "@chakra-ui/react";
 import { Meta } from "@storybook/react";
 
 import { ThemeProvider } from "../ThemeProvider";
@@ -14,6 +14,14 @@ export const Checkbox = () => (
       <ChakraCheckbox>Control button heading</ChakraCheckbox>
       <ChakraCheckbox disabled>Control button heading</ChakraCheckbox>
       <ChakraCheckbox isChecked>Control button heading</ChakraCheckbox>
+      <Box width="400px">
+        <ChakraCheckbox isChecked>
+          I understand the role of StarkNet delegates, we encourage all to read
+          the Delegate Expectations 328; Starknet Governance announcements Part
+          1 98, Part 2 44, and Part 3 34; The Foundation Post 60; as well as the
+          Delegate Onboarding announcement 539 before proceeding.
+        </ChakraCheckbox>
+      </Box>
     </VStack>
   </ThemeProvider>
 );
