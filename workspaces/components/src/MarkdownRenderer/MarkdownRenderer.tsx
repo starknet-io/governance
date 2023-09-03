59af6b50ecb37dba9e414ac813a36f7a44a174f7
Rob Walsh <r.walsh.21@gmail.com>
2023-09-03 13:39:08 +0100

    support headings in markdown renderer

--- workspaces/components/src/MarkdownRenderer/MarkdownRenderer.tsx
+++ workspaces/components/src/MarkdownRenderer/MarkdownRenderer.tsx
@@ -14,8 +14,12 @@ export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
   return (
     <ReactMarkdown
       components={{
-        h1: ({ ...props }) => <Heading as="h1" size="xl" {...props} />,
-        h2: ({ ...props }) => <Heading as="h2" size="xl" {...props} />,
+        h1: ({ ...props }) => <Heading variant="h1" as="h1" {...props} />,
+        h2: ({ ...props }) => <Heading variant="h2" as="h2" {...props} />,
+        h3: ({ ...props }) => <Heading variant="h3" as="h3" {...props} />,
+        h4: ({ ...props }) => <Heading variant="h4" as="h4" {...props} />,
+        h5: ({ ...props }) => <Heading variant="h5" as="h5" {...props} />,
+        h6: ({ ...props }) => <Heading variant="h6" as="h6" {...props} />,
         p: ({ ...props }) => <Text {...props} />,
         code: ({ ...props }) => <Code {...props} />,
         ol: ({ ...props }) => <ol style={{ paddingLeft: 16 }} {...props} />,
