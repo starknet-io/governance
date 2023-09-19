b2d46aee7cb30b81eccff7fc3cbacbc4554daa00
Akhmad Ounesoud <ahdmohd3@gmail.com>
2023-09-19 22:38:02 +0600

    * Edit order page * Some styling adjustments

---
+++ workspaces/frontend/src/pages/learn/reorder.page.tsx
@@ -0,0 +1,17 @@
+import {
+  Box,
+  ContentContainer,
+  Heading,
+} from "@yukilabs/governance-components";
+
+export function Page() {
+  return (
+    <ContentContainer>
+      <Box width="100%" maxWidth="538px" pb="200px" mx="auto">
+        <Heading variant="h3" mb="24px">
+          Edit pages order
+        </Heading>
+      </Box>
+    </ContentContainer>
+  );
+}
