ae5e7a642b445f2dee7ced410e6c7fe5f65d186a
Akhmad Ounesoud <ahdmohd3@gmail.com>
2023-09-19 22:50:04 +0600

    * ReOrder page

--- workspaces/frontend/src/pages/learn/reorder.page.tsx
+++ workspaces/frontend/src/pages/learn/reorder.page.tsx
@@ -1,16 +1,62 @@
 import {
   Box,
+  Button,
   ContentContainer,
+  Divider,
+  Flex,
   Heading,
+  MultiLevelReOrderableList,
 } from "@yukilabs/governance-components";
+import { TreeItems } from "@yukilabs/governance-components/src/MultiLevelReOrderableList/types";
+import { useEffect, useState } from "react";
+import { adaptTreeForFrontend, flattenTreeItems } from "src/utils/helpers";
+import { trpc } from "src/utils/trpc";
 
 export function Page() {
+  const [treeItems, setTreeItems] = useState<TreeItems>([]);
+
+  const { data, isLoading } = trpc.pages.getPagesTree.useQuery();
+  const { mutateAsync } = trpc.pages.savePagesTree.useMutation();
+
+  const pagesTree = data ?? [];
+
+  useEffect(() => {
+    if (pagesTree?.length && !treeItems.length) {
+      setTreeItems(adaptTreeForFrontend(pagesTree));
+    }
+  }, [pagesTree]);
+
+  const handleCancelClick = () => {
+    window.history.back();
+  };
+
+  const handleSaveChangesClick = () => {
+    mutateAsync(flattenTreeItems(treeItems), {
+      onSuccess: () => {
+        console.log("Success");
+      },
+    });
+  };
+
   return (
     <ContentContainer>
       <Box width="100%" maxWidth="538px" pb="200px" mx="auto">
         <Heading variant="h3" mb="24px">
           Edit pages order
         </Heading>
+        <Box>
+          <MultiLevelReOrderableList
+            items={treeItems}
+            setItems={setTreeItems}
+          />
+        </Box>
+        <Divider mt="14" mb="6" />
+        <Flex justifyContent="space-between">
+          <Button onClick={handleCancelClick} variant="ghost">
+            Cancel
+          </Button>
+          <Button onClick={handleSaveChangesClick}>Save changes</Button>
+        </Flex>
       </Box>
     </ContentContainer>
   );
