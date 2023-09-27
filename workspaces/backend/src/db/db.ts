bc2b4f66e9786943e6c6795a1201660bf90d4806
Rob Walsh <r.walsh.21@gmail.com>
2023-09-27 01:54:26 +0100

    modal style updates wip

--- workspaces/backend/src/db/db.ts
+++ workspaces/backend/src/db/db.ts
@@ -5,7 +5,7 @@ import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import dotenv from 'dotenv';
 
 import * as comments from './schema/comments';
-import * as commentVotes from "./schema/commentVotes";
+import * as commentVotes from './schema/commentVotes';
 import * as councils from './schema/councils';
 import * as delegates from './schema/delegates';
 import * as delegatesVotes from './schema/delegatesVotes';
@@ -15,18 +15,17 @@ import * as snips from './schema/snips';
 import * as users from './schema/users';
 import * as votes from './schema/votes';
 import * as usersToCouncils from './schema/usersToCouncils';
-import * as proposals from './schema/proposals'
+import * as proposals from './schema/proposals';
 import * as customDelegateAgreement from './schema/customDelegateAgreement';
 
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    ca: process.env.DO_CERT
-  },
+  // ssl: {
+  //   ca: process.env.DO_CERT
+  // },
 });
-
 
 const db = drizzle(pool, {
   schema: {
