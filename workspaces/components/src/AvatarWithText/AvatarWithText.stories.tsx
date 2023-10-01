8c6b8acd4b3e39c38874c4164b9ebdc77c890b6f
Rob Walsh <r.walsh.21@gmail.com>
2023-10-02 00:37:05 +0100

    fix story

--- workspaces/components/src/AvatarWithText/AvatarWithText.stories.tsx
+++ workspaces/components/src/AvatarWithText/AvatarWithText.stories.tsx
@@ -15,66 +15,6 @@ export default {
 export const Default = () => (
   <ThemeProvider>
     <Box>
-      <Flex
-        px="standard.xl"
-        bg="#fff"
-        maxWidth="372px"
-        pt="standard.3xl"
-        mb="200px"
-        flexDirection="column"
-        border="1px solid #eaeaea"
-      >
-        <AvatarWithText
-          size="standard"
-          src={
-            "https://i.insider.com/60f1a8567b0ec5001800a73e?width=1000&format=jpeg&auto=webp"
-          }
-          headerText="cillianhunter.eth"
-          subheaderText={truncateAddress(
-            "0x1234567890123456789012345678901234567890",
-          )}
-          address={"0x1234567890123456789012345678901234567890"}
-          dropdownChildren={
-            <>
-              <MenuItem>Item 1</MenuItem>
-              <MenuItem>Item 2</MenuItem>
-              <MenuItem>Item 3</MenuItem>
-            </>
-          }
-        />
-        <Box mt="standard.2xl">
-          <Button width="100%" variant="secondary" size="standard">
-            Undelegate your votes
-          </Button>
-        </Box>
-        <Box mt="standard.md">
-          <Banner
-            type="pending"
-            label="Your votes currently pending to this delegate"
-          />
-        </Box>
-        <Box pt="standard.2xl" pb="standard.2xl">
-          <SummaryItems.Root>
-            <SummaryItems.Item label="Proposals voted on" value={"2323"} />
-            <SummaryItems.Item
-              label="Delegated votes"
-              value={`${formatVotesAmount(3232)}`}
-            />
-            <SummaryItems.Item label="Total comments" value={"23"} />
-            <SummaryItems.Item
-              label="For/against/abstain"
-              value={`${23 ?? 0}/${3 ?? 0}/${4 ?? 0}`}
-            />
-            <SummaryItems.Item label="Delegation agreement" value={"none"} />
-            <SummaryItems.Item
-              isCopiable
-              isTruncated
-              label="Starknet address"
-              value={"0x1234567890123456789012345678901234567890"}
-            />
-          </SummaryItems.Root>
-        </Box>
-      </Flex>
       <VStack
         p={12}
         spacing={"22px"}
@@ -146,6 +86,66 @@ export const Default = () => (
           }
         />
       </VStack>
+      <Flex
+        px="standard.xl"
+        bg="#fff"
+        maxWidth="372px"
+        pt="standard.3xl"
+        mb="200px"
+        flexDirection="column"
+        border="1px solid #eaeaea"
+      >
+        <AvatarWithText
+          size="standard"
+          src={
+            "https://i.insider.com/60f1a8567b0ec5001800a73e?width=1000&format=jpeg&auto=webp"
+          }
+          headerText="cillianhunter.eth"
+          subheaderText={truncateAddress(
+            "0x1234567890123456789012345678901234567890",
+          )}
+          address={"0x1234567890123456789012345678901234567890"}
+          dropdownChildren={
+            <>
+              <MenuItem>Item 1</MenuItem>
+              <MenuItem>Item 2</MenuItem>
+              <MenuItem>Item 3</MenuItem>
+            </>
+          }
+        />
+        <Box mt="standard.2xl">
+          <Button width="100%" variant="secondary" size="standard">
+            Undelegate your votes
+          </Button>
+        </Box>
+        <Box mt="standard.md">
+          <Banner
+            type="pending"
+            label="Your votes currently pending to this delegate"
+          />
+        </Box>
+        <Box pt="standard.2xl" pb="standard.2xl">
+          <SummaryItems.Root>
+            <SummaryItems.Item label="Proposals voted on" value={"2323"} />
+            <SummaryItems.Item
+              label="Delegated votes"
+              value={`${formatVotesAmount(3232)}`}
+            />
+            <SummaryItems.Item label="Total comments" value={"23"} />
+            <SummaryItems.Item
+              label="For/against/abstain"
+              value={`${23 ?? 0}/${3 ?? 0}/${4 ?? 0}`}
+            />
+            <SummaryItems.Item label="Delegation agreement" value={"none"} />
+            <SummaryItems.Item
+              isCopiable
+              isTruncated
+              label="Starknet address"
+              value={"0x1234567890123456789012345678901234567890"}
+            />
+          </SummaryItems.Root>
+        </Box>
+      </Flex>
     </Box>
   </ThemeProvider>
 );
