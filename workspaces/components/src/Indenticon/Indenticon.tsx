f7b808abf12eaf730ad3d229f3dbff2f501a8111
Rob Walsh <r.walsh.21@gmail.com>
2023-10-02 16:40:32 +0100

    update identicon

--- workspaces/components/src/Indenticon/Indenticon.tsx
+++ workspaces/components/src/Indenticon/Indenticon.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
+import { Box, Image } from "@chakra-ui/react";
+import img from "./assets/star.svg";
 import { useColorGenerator } from "./useColorGenerator";
 type Props = {
   address?: string | null;
@@ -10,6 +10,17 @@ export const Indenticon = ({ address, size = 60 }: Props) => {
   const { svg } = useColorGenerator(`${address}`, size);
   return (
     <Box borderRadius="full" overflow="hidden" position="relative">
+      <Image
+        width={size}
+        position="absolute"
+        inset="0"
+        top="14px"
+        left="-4px"
+        src={img}
+        zIndex={"1"}
+        opacity="1"
+        blendMode={"color-dodge"}
+      />
       <div>
         <div dangerouslySetInnerHTML={{ __html: svg }} />
       </div>
