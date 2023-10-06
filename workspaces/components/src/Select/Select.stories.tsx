0cc835d776cb73b3dea81ed4630931644350ea0b
Rob Walsh <r.walsh.21@gmail.com>
2023-10-06 14:36:55 +0100

    Add new select component

---
+++ workspaces/components/src/Select/Select.stories.tsx
@@ -0,0 +1,151 @@
+import { Meta } from "@storybook/react";
+import { ThemeProvider } from "../ThemeProvider";
+import { Select } from "./Select";
+import { Box, HStack, Stack, Flex } from "@chakra-ui/react";
+import { FormControlled } from "src/FormControlled";
+import { Heading } from "src/Heading";
+import { Input } from "src/Input";
+import { SearchIcon } from "src/Icons";
+
+export default {
+  title: "governance-ui/Select",
+  component: Select,
+  Select,
+} as Meta<typeof Select>;
+
+const SORTING_OPTIONS = [
+  { label: "Newest", value: "desc" },
+  { label: "Oldest", value: "asc" },
+  { label: "Most discussed", value: "most_discussed" },
+];
+
+export const Default = () => (
+  <ThemeProvider>
+    <Flex justify="center" padding="standard.2xl">
+      <HStack spacing="56px" alignItems="start" wrap="wrap" maxWidth="600px">
+        <Stack minWidth="300px" width="100%" spacing="22px" alignItems="start">
+          <Heading variant="h3">Default Select</Heading>
+
+          <FormControlled
+            label="hello"
+            helperText="This is a not required field so..."
+          >
+            <Select
+              isRequired
+              isMulti={false}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+              placeholder="I'm a placeholder"
+            />
+          </FormControlled>
+
+          <FormControlled
+            label="hello"
+            helperText="This is a not required field so..."
+          >
+            <Input
+              placeholder="Type here..."
+              size="standard"
+              icon={<SearchIcon />}
+            />
+          </FormControlled>
+
+          <FormControlled
+            label="I'm a Select"
+            isRequired
+            isInvalid
+            errorMessage={"This is an error message"}
+          >
+            <Select
+              isInvalid
+              isMulti={false}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+          <FormControlled
+            label="I'm a Select"
+            isRequired
+            helperText="I am readonly or disabled"
+          >
+            <Select
+              placeholder="disabled"
+              isReadOnly={true}
+              isMulti={false}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+        </Stack>
+
+        <Stack minWidth="300px" width="100%" spacing="22px" alignItems="start">
+          <Heading variant="h3">Select sm</Heading>
+
+          <FormControlled
+            label="Multi Select (Small)"
+            helperText="This is a not required field so..."
+          >
+            <Select
+              size="sm"
+              isMulti={true}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+          <FormControlled
+            label="Invalid Small Select"
+            isInvalid
+            errorMessage="Error message for this select"
+          >
+            <Select
+              size="sm"
+              isInvalid={true}
+              isMulti={false}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+          <FormControlled
+            label="Readonly Small Select"
+            helperText="This is a not required field so..."
+          >
+            <Select
+              size="sm"
+              isReadOnly={true}
+              isMulti={false}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+        </Stack>
+
+        <Stack minWidth="300px" width="100%" spacing="22px" alignItems="start">
+          <Heading variant="h3">Multi select</Heading>
+          <FormControlled
+            label="Multi Select"
+            helperText="This is a not required field so..."
+          >
+            <Select
+              tagVariant="select"
+              isMulti={true}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+          <FormControlled
+            label="Multi Select (Small)"
+            helperText="This is a not required field so..."
+          >
+            <Select
+              size="sm"
+              tagVariant="select"
+              isMulti={true}
+              onChange={() => console.log("changed")}
+              options={SORTING_OPTIONS}
+            />
+          </FormControlled>
+        </Stack>
+      </HStack>
+    </Flex>
+  </ThemeProvider>
+);
---
+++ workspaces/components/src/Select/Select.tsx
@@ -0,0 +1,62 @@
+import {
+  Select as CustomSelect,
+  ChakraStylesConfig,
+  Props,
+} from "chakra-react-select";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+export interface SelectProps extends Props {
+  options: Option[] | Option;
+  value?: string[];
+  onChange: (values: string[]) => void;
+}
+
+export const Select = ({
+  isInvalid = false,
+  isReadOnly = false,
+  isMulti = false,
+  size,
+  ...rest
+}: SelectProps & { size?: "sm" | "md" }) => {
+  const chakraStyles: ChakraStylesConfig = {
+    container: (provided) => ({
+      ...provided,
+    }),
+
+    valueContainer: (provided) => ({
+      ...provided,
+
+      paddingInlineStart: "8px",
+      paddingInlineEnd: "8px",
+    }),
+    option: (provided, { isSelected }) => ({
+      ...provided,
+      fontSize: "14px",
+      fontWeight: "400",
+      lineHeight: "20px",
+      letterSpacing: "0.07px",
+      fontFamily: "Inter Variable",
+      ...(isSelected && {
+        bg: "surface.forms.hover",
+        color: "content-accent-default",
+      }),
+    }),
+  };
+
+  return (
+    <CustomSelect
+      chakraStyles={chakraStyles}
+      isMulti={isMulti}
+      size={size}
+      tagVariant="select"
+      isInvalid={isInvalid}
+      isReadOnly={isReadOnly}
+      variant="primary"
+      {...rest}
+    />
+  );
+};
