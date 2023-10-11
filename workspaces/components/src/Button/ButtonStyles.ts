3ff9365013ca5383fef4da9d81fc4dcf845a7e3f
Rob Walsh <r.walsh.21@gmail.com>
2023-10-12 00:56:15 +0100

    Fix filters button

--- workspaces/components/src/Button/ButtonStyles.ts
+++ workspaces/components/src/Button/ButtonStyles.ts
@@ -35,9 +35,8 @@ export const buttonTheme = {
       },
     },
     withBadgeCondensed: {
-      minWidth: "36px",
       paddingLeft: "condensed.lg!important",
-      paddingRight: "40px!important",
+      paddingRight: "condensed.lg!important",
       borderRadius: "condensed.base",
       py: "condensed.sm",
       minHeight: "36px",
