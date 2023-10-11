3b12ed6487a536aa7097d4f8874d6d6d436875f9
Rob Walsh <r.walsh.21@gmail.com>
2023-10-11 16:47:06 +0100

    control order of field errors

--- workspaces/components/src/FormControlled/useFormErrorHandler.ts
+++ workspaces/components/src/FormControlled/useFormErrorHandler.ts
@@ -8,16 +8,31 @@ type Errors = {
   [key: string]: any;
 };
 
-export function useFormErrorHandler() {
+export function useFormErrorHandler(fieldOrder: string[]) {
   const refs = useRef<ErrorRefs>({});
+
   const setErrorRef = useCallback((name: string, ref: HTMLElement | null) => {
     refs.current[name] = ref;
   }, []);
 
-  const scrollToError = useCallback((errors: Errors) => {
-    const firstErrorKey = Object.keys(errors)[0];
-    refs.current[firstErrorKey]?.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  const getFirstErrorField = (errors: Errors): string | null => {
+    for (const field of fieldOrder) {
+      if (errors[field]) {
+        return field;
+      }
+    }
+    return null;
+  };
+
+  const scrollToError = useCallback(
+    (errors: Errors) => {
+      const firstErrorField = getFirstErrorField(errors);
+      if (firstErrorField) {
+        refs.current[firstErrorField]?.scrollIntoView({ behavior: "smooth" });
+      }
+    },
+    [fieldOrder],
+  );
 
   return { setErrorRef, scrollToError };
 }
