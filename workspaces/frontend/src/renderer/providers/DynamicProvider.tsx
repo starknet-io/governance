c89bf63b41ed8ce122f542eae766271534e51c71
Rob Walsh <r.walsh.21@gmail.com>
2023-10-18 00:50:14 +0100

    Move  Profile menu to new dynamic provider

--- workspaces/frontend/src/renderer/providers/DynamicProvider.tsx
+++ workspaces/frontend/src/renderer/providers/DynamicProvider.tsx
@@ -8,7 +8,9 @@ import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { trpc } from "src/utils/trpc";
 import { cssOverrides } from "src/components/style/overrides";
-
+import { ProfileInfoModal } from "@yukilabs/governance-components";
+import { useFileUpload } from "src/hooks/useFileUpload";
+import { usePageContext } from "../PageContextProvider";
 interface Props {
   // readonly pageContext: PageContext;
   readonly children: React.ReactNode;
@@ -26,13 +28,16 @@ interface AuthSuccessParams {
 export const DynamicProvider = (props: Props) => {
   const { children } = props;
   const [authUser, setAuthUser] = useState<AuthSuccessParams | null>(null);
+  const [userExistsError, setUserExistsError] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const authMutation = trpc.auth.authUser.useMutation();
   const logoutMutation = trpc.auth.logout.useMutation();
-  const hasCalledAuthenticateUser = useRef(false);
-  const [modalOpen, setModalOpen] = useState(false);
 
-  const editUserProfile = trpc.users.editUserProfileByAddress.useMutation();
+  const editUserProfile = trpc.users.editUserProfile.useMutation();
+  const hasCalledAuthenticateUser = useRef(false);
   const utils = trpc.useContext();
+  const { user } = usePageContext();
+  const { handleUpload } = useFileUpload();
 
   const authenticateUser = useCallback(
     async (params: AuthSuccessParams) => {
@@ -58,6 +63,45 @@ export const DynamicProvider = (props: Props) => {
     }
   }, [authUser, authenticateUser]);
 
+  async function handleSave(data: {
+    username: string;
+    starknetAddress: string;
+    profileImage: string | null;
+  }): Promise<any> {
+    if (!user) {
+      return false;
+    }
+    try {
+      const res = await editUserProfile.mutateAsync(
+        {
+          id: user.id,
+          username: data.username !== user?.username ? data.username : null,
+          starknetAddress: data.starknetAddress,
+          profileImage: data.profileImage,
+        },
+        {
+          onSuccess: () => {
+            utils.auth.currentUser.invalidate();
+            setModalOpen(false);
+            return true;
+          },
+          onError: (error) => {
+            if (error.message === "Username already exists") {
+              setUserExistsError(true);
+            }
+            return false;
+          },
+        },
+      );
+      return res;
+    } catch (error) {
+      return false;
+    }
+  }
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   const handleDynamicLogout = () => {
     logoutMutation.mutateAsync(undefined, {
       onSuccess: () => {
@@ -67,22 +111,34 @@ export const DynamicProvider = (props: Props) => {
     });
   };
   return (
-    <DynamicContextProvider
-      settings={{
-        environmentId: import.meta.env.VITE_APP_DYNAMIC_ID,
-        eventsCallbacks: {
-          onAuthSuccess: (params: AuthSuccessParams) => {
-            setAuthUser(params);
+    <>
+      <ProfileInfoModal
+        isOpen={modalOpen}
+        onClose={handleModalClose}
+        user={user}
+        mode="create"
+        handleUpload={handleUpload}
+        saveData={(data) => handleSave(data)}
+        userExistsError={userExistsError}
+        setUsernameErrorFalse={() => setUserExistsError(false)}
+      />
+      <DynamicContextProvider
+        settings={{
+          environmentId: import.meta.env.VITE_APP_DYNAMIC_ID,
+          eventsCallbacks: {
+            onAuthSuccess: (params: AuthSuccessParams) => {
+              setAuthUser(params);
+            },
+            onLogout: () => handleDynamicLogout(),
           },
-          onLogout: () => handleDynamicLogout(),
-        },
-        cssOverrides,
-      }}
-    >
-      <DynamicWagmiConnector>
-        {children}
-        {/* <LayoutDefault pageContext={pageContext}>{children}</LayoutDefault> */}
-      </DynamicWagmiConnector>
-    </DynamicContextProvider>
+          cssOverrides,
+        }}
+      >
+        <DynamicWagmiConnector>
+          {children}
+          {/* <LayoutDefault pageContext={pageContext}>{children}</LayoutDefault> */}
+        </DynamicWagmiConnector>
+      </DynamicContextProvider>
+    </>
   );
 };
