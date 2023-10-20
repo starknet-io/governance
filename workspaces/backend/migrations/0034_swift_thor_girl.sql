e656e716478681d3bb586e72488f8470c24ed004
xgrizx <xxxgrizxxx@gmail.com>
2023-10-20 11:05:42 +0200

    migrations

---
+++ workspaces/backend/migrations/0034_swift_thor_girl.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS "notifications" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"message" text NOT NULL,
+	"type" text NOT NULL,
+	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+	"updatedAt" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "notification_users" (
+	"notificationId" uuid,
+	"userId" uuid,
+	"read" boolean DEFAULT false NOT NULL
+);
+
+DO $$ BEGIN
+ ALTER TABLE "notification_users" ADD CONSTRAINT "notification_users_notificationId_notifications_id_fk" FOREIGN KEY ("notificationId") REFERENCES "notifications"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "notification_users" ADD CONSTRAINT "notification_users_userId_delegates_id_fk" FOREIGN KEY ("userId") REFERENCES "delegates"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
