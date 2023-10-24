e84562e454e3855719a6c20fa923d915dd0ff0a4
xgrizx <xxxgrizxxx@gmail.com>
2023-10-24 16:40:14 +0200

    revert fe to original port

--- workspaces/frontend/src/server/index.cjs
+++ workspaces/frontend/src/server/index.cjs
@@ -41,7 +41,7 @@ async function startServer() {
     httpResponse.pipe(res)
   })
 
-  const port = process.env.PORT || 3001
+  const port = process.env.PORT || 3000
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
 }
