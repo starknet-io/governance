d5ba13d641f9d224f25fe53a47027fd9f68ea91a
bgharbi <badreddine.gharbi@toptal.com>
2023-10-31 20:44:01 +0100

    add dockerfile and ssl config

--- workspaces/snapshot-webhook/src/helpers/mysql.ts
+++ workspaces/snapshot-webhook/src/helpers/mysql.ts
@@ -3,6 +3,7 @@ import Pool from 'mysql/lib/Pool';
 import Connection from 'mysql/lib/Connection';
 import bluebird from 'bluebird';
 import parse from 'connection-string';
+import fs from 'fs'
 
 const connectionLimit = parseInt(process.env.CONNECTION_LIMIT || '5');
 
@@ -13,11 +14,11 @@ config.multipleStatements = true;
 config.database = config.path[0];
 config.host = config.hosts[0].name;
 config.port = config.hosts[0].port;
-config.password = '';
 config.connectTimeout = 60e3;
 config.acquireTimeout = 60e3;
 config.timeout = 60e3;
 config.charset = 'utf8mb4';
+config.ssl = {ca: fs.readFileSync("ca-certificate.crt").toString()};
 bluebird.promisifyAll([Pool, Connection]);
 const db = mysql.createPool(config);
 
