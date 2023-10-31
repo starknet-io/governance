6715804a6e8bc5b1cc405538c7240b899823e7df
xgrizx <xxxgrizxxx@gmail.com>
2023-10-31 19:25:52 +0100

    syncing SS -> gov done

--- workspaces/backend/src/scripts/syncSnapshotUsers.ts
+++ workspaces/backend/src/scripts/syncSnapshotUsers.ts
@@ -1,13 +1,36 @@
-import { graphqlClient } from '../utils/graphqlClient';
+import { GraphQLClient } from 'graphql-request';
+import { db } from '../db/db';
+import { eq } from 'drizzle-orm';
+import { users } from '../db/schema/users';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const endpoint = 'https://hub.snapshot.org/graphql';
+
+const graphqlClient = new GraphQLClient(endpoint, {
+  headers: {
+    'x-api-key': process.env.SNAPSHOT_API_KEY!,
+  },
+});
+
+const rolesDictionary: any = {
+  admins: 'admin',
+  moderators: 'moderator',
+  members: 'user',
+};
 
 interface SpaceInterface {
-  id: string
-  name: string
-  about: string
-  network: string
-  symbol: string
-  members: string[]
+  id: string;
+  name: string;
+  about: string;
+  network: string;
+  symbol: string;
+  members: string[];
+  admins: string[];
+  moderators: string[];
 }
+
 async function fetchAllUsersFromSpace() {
   const query = `
     query {
@@ -18,24 +41,53 @@ async function fetchAllUsersFromSpace() {
         network
         symbol
         members
+        admins
+        moderators
       }
     }
   `;
 
-  const response: { space: SpaceInterface } = await graphqlClient.request(query);
-  return response?.space?.members || [];
+  const response: { space: SpaceInterface } =
+    await graphqlClient.request(query);
+  const allUsers = ['members', 'admins', 'moderators'].reduce(
+    (acc: any, role) => {
+      const castedRole = role as 'members' | 'admins' | 'moderators';
+      const userArray = response?.space?.[castedRole] || [];
+      const usersWithRole =
+        userArray?.map((address: string) => ({
+          address,
+          role: rolesDictionary[role] as 'user' | 'moderator' | 'admin',
+        })) || [];
+      return acc.concat(usersWithRole);
+    },
+    [],
+  );
+
+  return allUsers;
 }
-const syncSnapshotUsers = async() => {
-  console.log('First, get all users from snapshot')
+
+const syncSnapshotUsers = async () => {
+  console.log('First, get all users from snapshot');
   const allSnapshotSpaceUsers = await fetchAllUsersFromSpace();
-  console.log('Now, for each user obtain a role')
+  console.log('Now, for each user obtain a role');
   for (const user of allSnapshotSpaceUsers) {
-    console.log(user)
-    // Check if user already exists
-
-    // If yes, then update role
-
-    // If no, then create user with role
+    console.log('processing', user);
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.address, user.address.toLowerCase()),
+    });
+    // If user exists, update role
+    if (existingUser) {
+      await db.update(users).set({
+        role: user.role,
+      });
+    } else {
+      // if user is new, create one
+      await db.insert(users).values({
+        address: user.address.toLowerCase(), // save address as lowercase
+        role: user.role,
+      });
+    }
   }
+  console.log('Done processing');
 };
 export default syncSnapshotUsers();
