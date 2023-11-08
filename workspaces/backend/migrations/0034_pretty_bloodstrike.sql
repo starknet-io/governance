3fa6bb1fc86305c5bc8fb620dcd6a99dafb0c34d
xgrizx <xxxgrizxxx@gmail.com>
2023-11-08 11:27:07 +0100

    migration changes

--- workspaces/backend/migrations/0034_pretty_bloodstrike.sql
+++ workspaces/backend/migrations/0034_pretty_bloodstrike.sql
@@ -20,6 +20,13 @@ CREATE TABLE IF NOT EXISTS "notification_users" (
 	"read" boolean DEFAULT false NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS "subscribers" (
+	"email" text NOT NULL,
+	"userId" uuid,
+	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+	"updatedAt" timestamp with time zone DEFAULT now() NOT NULL
+);
+
 DO $$ BEGIN
  ALTER TABLE "notifications" ADD CONSTRAINT "notifications_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
 EXCEPTION
@@ -46,6 +53,12 @@ END $$;
 
 DO $$ BEGIN
  ALTER TABLE "notification_users" ADD CONSTRAINT "notification_users_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+ ALTER TABLE "subscribers" ADD CONSTRAINT "subscribers_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE cascade ON UPDATE cascade;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
