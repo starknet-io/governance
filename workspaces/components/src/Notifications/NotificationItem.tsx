416e6885269375e1b38d921ce52f1487142451f4
xgrizx <xxxgrizxxx@gmail.com>
2023-11-09 18:54:39 +0100

    update notification item

--- workspaces/components/src/Notifications/NotificationItem.tsx
+++ workspaces/components/src/Notifications/NotificationItem.tsx
@@ -1,5 +1,11 @@
 import { truncateAddress } from "../utils";
-import { MenuItem, Text } from "../../index";
+import {
+  MenuItem,
+  Text,
+  VoteAbstainIcon,
+  VoteAgainstIcon,
+  VoteForIcon,
+} from "../../index";
 import { Flex, Icon, Box, Avatar } from "@chakra-ui/react";
 import { Indenticon } from "../Indenticon";
 import {
@@ -32,16 +38,63 @@ type NotificationItemProps = {
   onMarkNotificationRead: (notificationId: string) => void;
 };
 
+const findIndexOfLargest = (scores: Array<number>) => {
+  let maxIndex = 0; // Start with the first index
+  let maxValue = scores[0]; // And the first value
+
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > maxValue) {
+      maxIndex = i;
+      maxValue = scores[i];
+    }
+  }
+
+  return maxIndex; // Return the index of the largest number
+};
+
 export const NotificationItem = ({
   notification,
   onMarkNotificationRead,
 }: NotificationItemProps) => {
   const formattedAddress =
-    notification?.user?.username || notification?.user?.ensName || truncateAddress(notification?.user?.address);
+    notification?.user?.username ||
+    notification?.user?.ensName ||
+    truncateAddress(notification?.user?.address);
   const isProposal = notification.type.includes("proposal");
+  const isPost = notification.postId;
   const isComment = notification.type.includes("comment");
+  const isProposalCreate = notification.type === "proposal/created";
+  const isProposalStart = notification.type === "proposal/start";
+  const isProposalEnd = notification.type === "proposal/end";
+  const getProposalResultAsIcon = () => {
+    const { proposal } = notification;
+    if (!proposal) {
+      return "";
+    }
+    const result = findIndexOfLargest(proposal.scores);
+    if (result === 0) {
+      return <VoteForIcon color="#44D095" boxSize="20px" />;
+    }
+    if (result === 1) {
+      return <VoteAgainstIcon color="#EC796B" boxSize="20px" />;
+    }
+    if (result === 2) {
+      return <VoteAbstainIcon color="#4A4A4F" boxSize="20px" />;
+    }
+    return <VoteForIcon color="#44D095" boxSize="20px" />;
+  };
   const formattedType = () => {
-    return isComment ? "replied on your comment" : notification.type;
+    if (isComment) {
+      return `replied on your comment on ${isPost ? "post" : "proposal"}:`;
+    } else if (isProposalStart) {
+      return "Voting is active on proposal:";
+    } else if (isProposalCreate) {
+      return "added a new proposal:";
+    } else if (isProposalEnd) {
+      return "Voting has ended with majority vote of";
+    }
+
+    return notification.type;
   };
   const avatar =
     notification?.user?.profileImage || notification?.user?.ensAvatar;
@@ -52,6 +105,7 @@ export const NotificationItem = ({
       notification.title
     );
   };
+  const notProposalStartOrEnd = !isProposalEnd && !isProposalStart
   return (
     <MenuItem
       maxW={"400px"}
@@ -70,11 +124,20 @@ export const NotificationItem = ({
       }}
     >
       <Flex gap={1.5} width="100%">
-        <Flex direction="column" alignItems="center" gap={1}>
-          {avatar ? (
-            <Avatar width={8} height={8} src={avatar} />
+        <Flex direction="column" alignItems="center" gap={notProposalStartOrEnd ? 1 : 0}>
+          {notProposalStartOrEnd ? (
+            <>
+              {avatar ? (
+                <Avatar width={8} height={8} src={avatar} />
+              ) : (
+                <Indenticon
+                  size={32}
+                  address={notification?.user?.address?.toLowerCase()}
+                />
+              )}
+            </>
           ) : (
-            <Indenticon size={32} address={notification?.user?.address?.toLowerCase()} />
+            <Box width={8}></Box>
           )}
           <Box color="content.support.default">
             <Icon
@@ -89,24 +152,45 @@ export const NotificationItem = ({
           </Box>
         </Flex>
         <Flex direction="column" alignItems="flex-start" flex="1">
-          <Box display="inline-block">
-            <Text ml={1} variant="bodyMediumStrong" display="inline-block">
-              {formattedAddress}
-            </Text>
+          <Box ml={1}>
+            {!isProposalStart && !isProposalEnd ? (
+              <>
+                <Text as="span" variant="bodyMediumStrong">
+                  {formattedAddress}
+                </Text>{" "}
+                {/* This space is important to separate inline elements */}
+              </>
+            ) : null}
             <Text
-              ml={1}
+              as="span"
               variant="mediumStrong"
               color="content.support.default"
-              display="inline-block"
             >
-              {formattedType()}:
-            </Text>
-            <Text ml={1} variant="mediumStrong" color="content.default.default">
+              {formattedType()}
+            </Text>{" "}
+            {isProposalEnd && (
+              <>
+                <Icon as={() => getProposalResultAsIcon()} />{" "}
+                <Text
+                  as="span"
+                  variant="mediumStrong"
+                  color="content.support.default"
+                >
+                  on proposal:
+                </Text>{" "}
+              </>
+            )}
+            {/* This space is important to separate inline elements */}
+            <Text
+              as="span"
+              variant="mediumStrong"
+              color="content.default.default"
+            >
               “{getTitle()}“
             </Text>
           </Box>
           <Text variant="small" color="content.support.default" ml={1}>
-            {daysAgo(notification.createdAt)}
+            {daysAgo(notification.createdAt)} ago
           </Text>
         </Flex>
         <Flex alignSelf="center" justifySelf="flex-end">
