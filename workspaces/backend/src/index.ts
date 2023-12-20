d73e5030fac3ca091ceeb599edb51da9f128060f
xgrizx <xxxgrizxxx@gmail.com>
2023-12-20 12:29:48 +0100

    initial round

--- workspaces/backend/src/index.ts
+++ workspaces/backend/src/index.ts
@@ -10,24 +10,44 @@ import { users } from './db/schema/users';
 import { eq } from 'drizzle-orm';
 import { getUserByJWT } from './utils/helpers';
 import multer from 'multer';
-import { delegateRouter } from "./routers/delegates";
-import {notificationsRouter} from "./routers/notifications";
+import { delegateRouter } from './routers/delegates';
+import { notificationsRouter } from './routers/notifications';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import url from 'url';
+import morgan from 'morgan';
+
+// 15 mins -> 250 reqs
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 250,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false
+});
 
 dotenv.config();
 
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
-const fetchUserMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+const fetchUserMiddleware = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
   if (req.cookies?.JWT) {
-    const userId: string | undefined = (await getUserByJWT(req.cookies.JWT))?.id
+    const userId: string | undefined = (await getUserByJWT(req.cookies.JWT))
+      ?.id;
     if (!userId) {
       return next();
     }
-    const user = await db.query.users.findFirst({ where: eq(users.id, userId), with: { delegationStatement: true } });
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, userId),
+      with: { delegationStatement: true },
+    });
     if (user) {
       req.user = user;
     }
@@ -37,20 +57,45 @@ const fetchUserMiddleware = async (req: express.Request, res: express.Response, 
 
 app.use(cookieParser());
 app.use(fetchUserMiddleware);
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', credentials: true }));
+app.use(limiter);
+morgan.token('decoded-url', (req) => decodeURIComponent(req.originalUrl));
 
-app.use("/trpc", upload.single('file'), createExpressMiddleware({
-  router: appRouter,
-  createContext
-}));
-app.use('/api/delegates', createExpressMiddleware({
-  router: delegateRouter,
-  createContext
-}));
-app.use('/api/notifications', createExpressMiddleware({
-  router: notificationsRouter,
-  createContext
-}));
+app.use(morgan(':method :decoded-url :status :response-time ms', {
+  skip: function (req, res) {
+    return res.statusCode < 400;  // Skip logging if status code is less than 400
+  }
+}));app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+
+app.get('/health', (req, res) => res.sendStatus(200));
+
+app.use(
+  '/trpc',
+  upload.single('file'),
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+app.use(
+  '/api/delegates',
+  createExpressMiddleware({
+    router: delegateRouter,
+    createContext,
+  }),
+);
+app.use(
+  '/api/notifications',
+  createExpressMiddleware({
+    router: notificationsRouter,
+    createContext,
+  }),
+);
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
