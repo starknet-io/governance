a68ce90bfa91a339478e8cd779f403bedadf8223
xgrizx <xxxgrizxxx@gmail.com>
2023-12-26 10:17:37 +0100

    fix issue with delegation

--- workspaces/components/src/UserProfileMenu/UserProfileMenu.tsx
+++ workspaces/components/src/UserProfileMenu/UserProfileMenu.tsx
@@ -54,7 +54,7 @@ export const UserProfileContent: React.FC<UserProfileMenuProps> = ({
   handleOpenModal,
   setEditUserProfile,
 }) => {
-  const delegatedToName = delegatedTo.username || delegatedTo.ensName
+  const delegatedToName = delegatedTo?.username || delegatedTo?.ensName
   return (
     <>
       <Box position="absolute" right="12px" top="12px" zIndex={0}>
