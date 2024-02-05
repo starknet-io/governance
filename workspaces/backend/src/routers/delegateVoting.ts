e91c9383f24ce4da52a6d746bef881a202b36909
xgrizx <xxxgrizxxx@gmail.com>
2024-02-05 17:10:13 +0100

    reworked cron job

--- workspaces/backend/src/routers/delegateVoting.ts
+++ workspaces/backend/src/routers/delegateVoting.ts
@@ -1,78 +1,49 @@
-import { graphqlClient } from '../utils/graphqlClient';
+import { GraphQLClient } from 'graphql-request';
 import { db } from '../db/db';
 import { delegateVotes } from '../db/schema/delegatesVotes';
 import { eq } from 'drizzle-orm';
+import axios from 'axios';
 
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const graphQLClientL2 = new GraphQLClient(
+  'https://starknet-delegates.checkpoint.fyi',
+  {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  },
+);
 
-export async function saveDelegateVotes(delegateDataBatch: any) {
+const L1_ENDPOINT =
+  'https://api.studio.thegraph.com/query/23545/sepolia-delegates/version/latest';
+
+export async function saveDelegateVotes(delegateData: any) {
   try {
-    for (const delegateData of delegateDataBatch) {
-      const existingRecord = await db.query.delegateVotes.findFirst({
-        where: eq(delegateVotes.delegateId, delegateData.delegateId),
-      });
+    const existingRecord = await db.query.delegateVotes.findFirst({
+      where: eq(delegateVotes.delegateId, delegateData.delegateId),
+    });
 
-      if (existingRecord) {
-        await db
-          .update(delegateVotes)
-          .set({
-            votingPower: delegateData.votingPower,
-            totalVotes: delegateData.totalVotes,
-            updatedAt: new Date(),
-          })
-          .where(eq(delegateVotes.delegateId, delegateData.delegateId));
-      } else {
-        await db.insert(delegateVotes).values({
-          delegateId: delegateData.delegateId,
-          address: delegateData.address,
+    if (existingRecord) {
+      await db
+        .update(delegateVotes)
+        .set({
           votingPower: delegateData.votingPower,
           totalVotes: delegateData.totalVotes,
           updatedAt: new Date(),
-        });
-      }
+        })
+        .where(eq(delegateVotes.delegateId, delegateData.delegateId));
+    } else {
+      await db.insert(delegateVotes).values({
+        delegateId: delegateData.delegateId,
+        address: delegateData.address,
+        votingPower: delegateData.votingPower,
+        totalVotes: delegateData.totalVotes,
+        updatedAt: new Date(),
+      });
     }
   } catch (error) {
     console.error('Error saving delegate votes:', error);
     throw error;
   }
-}
-
-async function fetchVotingPower(delegateId: string) {
-  const query = `
-    query VotingPower {
-      vp (
-        voter: "${delegateId}"
-        space: "starknet.eth"
-      ) {
-        vp
-        vp_by_strategy
-        vp_state
-      }
-    }
-  `;
-
-  const response: { vp: number } = await graphqlClient.request(query);
-  return response.vp;
-}
-
-async function fetchtotalVotes(delegateId: string) {
-  const query = `
-    query Vote {
-      votes (
-        where: {
-          voter: "${delegateId}",
-          space: "starknet.eth"
-        }
-      ) {
-        id
-      }
-    }
-  `;
-
-  const response: { votes: any[] } = await graphqlClient.request(query);
-  return response.votes.length;
 }
 
 interface DelegateData {
@@ -82,123 +53,52 @@ interface DelegateData {
   address: string;
 }
 
-async function fetchDelegateData(delegate: {
-  address: string;
+interface DelegateDataSnapshot {
+  delegatedVotes: string;
   id: string;
-}): Promise<DelegateData> {
-  try {
-    const [votingPower, totalVotes] = await Promise.all([
-      fetchVotingPower(delegate.address),
-      fetchtotalVotes(delegate.address),
-    ]);
-
-    return {
-      delegateId: delegate.id,
-      address: delegate.address,
-      // @ts-ignore
-      votingPower: votingPower?.vp || 0,
-      totalVotes,
-    };
-  } catch (error) {
-    console.log(
-      `Error fetching delegate data for delegate: ${delegate.address}:`,
-      error,
-    );
-    throw error;
-  }
 }
 
-async function processDelegatesInBatches(
-  delegates: { address: string; id: string }[],
-  batchSize: number,
-) {
-  let batchStart = 0;
-  let failedBatches: { address: string; id: string }[] = [];
-  const apiCallLimit = 24; // To stay below 64 API calls within 16 seconds - lowered to 58 just in case
-  let apiCallCount = 0;
-
-  while (batchStart < delegates.length) {
-    const batchStartTime = Date.now();
-    const batchEnd = Math.min(batchStart + batchSize, delegates.length);
-    console.log(batchEnd, batchStart, delegates.length);
-    const delegateBatch = delegates.slice(batchStart, batchEnd);
-    apiCallCount += delegateBatch.length;
-
-    if (apiCallCount >= apiCallLimit) {
-      console.log('Approaching rate limit, pausing for 5 seconds...');
-      await delay(6000); // 5 seconds pause
-      apiCallCount = 0; // Reset the API call count
-    }
-
-    try {
-      const delegateDataPromises = delegateBatch.map((delegate) =>
-        fetchDelegateData(delegate),
-      );
-      const delegateDataBatch = await Promise.all(delegateDataPromises);
-
-      await saveDelegateVotes(delegateDataBatch);
-      console.log(delegateDataBatch);
-    } catch (error) {
-      console.log('Error processing delegate batch:', error);
-      failedBatches = failedBatches.concat(delegateBatch);
-    }
-
-    batchStart = batchEnd;
-    const batchEndTime = Date.now();
-    const timePassed = (batchEndTime - batchStartTime) / 1000;
-
-    console.log(`Time passed for this batch: ${timePassed} seconds`);
-  }
-
-  let retryCount = 0;
-  const maxRetryCount = 10; // Set a max retry count to prevent infinite loop
-  while (failedBatches.length > 0 && retryCount < maxRetryCount) {
-    console.log(`Retrying failed batches: Attempt ${retryCount + 1}`);
-    console.log(`Number of failed entries: ${failedBatches.length}`);
-
-    const newFailedBatches: { address: string; id: string }[] = [];
-    batchStart = 0;
-    apiCallCount = 0; // Reset API call count before the retry loop
-
-    while (batchStart < failedBatches.length) {
-      const batchEndTime = Math.min(
-        batchStart + batchSize,
-        failedBatches.length,
-      );
-      const retryBatch = failedBatches.slice(batchStart, batchEndTime);
-
-      apiCallCount += retryBatch.length;
-
-      if (apiCallCount >= apiCallLimit) {
-        console.log(
-          'Approaching rate limit during retry, pausing for 5 seconds...',
-        );
-        await delay(5000); // 5 seconds pause
-        apiCallCount = 0; // Reset the API call count
+async function fetchAllDelegatesFromSnapshot({
+  isL1,
+  isL2,
+}: {
+  isL1?: boolean;
+  isL2?: boolean;
+}) {
+  const query = `
+    query Delegates {
+      delegates(orderBy: delegatedVotes, orderDirection: desc) {
+        id
+        delegatedVotes
+        delegatedVotesRaw
       }
-
-      const retryPromises = retryBatch.map((failedBatch) =>
-        fetchDelegateData(failedBatch)
-          .then((delegateData) => saveDelegateVotes([delegateData]))
-          .catch((error) => {
-            console.log('Error processing failed batch on retry:', error);
-            newFailedBatches.push(failedBatch);
-          }),
-      );
-
-      await Promise.all(retryPromises);
-
-      batchStart = batchEndTime;
     }
-
-    failedBatches = newFailedBatches;
-    retryCount += 1;
-  }
-
-  if (failedBatches.length === 0) {
-    console.log('Job finished');
-  } else {
-    console.error('Job finished with errors');
+  `;
+  try {
+    if (isL1) {
+      const response = await axios.post(
+        L1_ENDPOINT,
+        {
+          query: query,
+          operationName: 'Delegates',
+          extensions: {},
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      return response.data.data.delegates;
+    } else if (isL2) {
+      const response: any = await graphQLClientL2.request(query);
+      return response?.delegates || [];
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.log('Error fetching delegates:', error);
+    throw error;
   }
 }
 
@@ -212,13 +112,15 @@ async function fetchAllDelegates() {
         author: {
           columns: {
             address: true,
+            ethereumAddress: true,
+            starknetAddress: true,
           },
         },
       },
     });
     const parsedDelegates = delegates.map((delegate: any) => ({
       id: delegate.id,
-      address: delegate?.author?.address || "",
+      address: delegate?.author?.address || '',
     }));
     return parsedDelegates;
   } catch (error) {
@@ -227,11 +129,97 @@ async function fetchAllDelegates() {
   }
 }
 
+async function fetchAllDelegatesPaginated(pageSize: number, page: number) {
+  try {
+    // Fetch a page of delegates
+    const delegates: any = await db.query.delegates.findMany({
+      limit: pageSize,
+      offset: page * pageSize,
+      columns: {
+        id: true,
+      },
+      with: {
+        author: {
+          columns: {
+            address: true,
+            ethAddress: true,
+            starknetAddress: true,
+          },
+        },
+      },
+    });
+    const parsedDelegates = delegates.map((delegate: any) => ({
+      id: delegate.id,
+      address: delegate?.author?.address || null,
+      // if eth address not specified and address is different from starknet, it must belong to eth
+      ethAddress:
+        delegate?.author?.ethAddress ||
+        (delegate?.author?.starknetAddress !== delegate?.author?.address
+          ? delegate?.author?.address
+          : null),
+      starknetAddress: delegate?.author?.starknetAddress || null,
+    }));
+    return parsedDelegates;
+  } catch (error) {
+    console.error('Error fetching delegates:', error);
+    throw error;
+  }
+}
+
 export async function delegateVoting() {
-  const delegates: { address: string, id: string}[] = await fetchAllDelegates();
-  await processDelegatesInBatches(delegates, 5);
+  const pageSize = 100;
+  let page = 0;
+  let hasMore = true;
+
+  const delegatesSnapshotL1: DelegateDataSnapshot[] =
+    await fetchAllDelegatesFromSnapshot({ isL1: true });
+  const delegatesSnapshotL2: DelegateDataSnapshot[] =
+    await fetchAllDelegatesFromSnapshot({ isL2: true });
+
+  console.log(delegatesSnapshotL1);
+  console.log(delegatesSnapshotL2);
+
+  while (hasMore) {
+    const localDelegatesBatch = await fetchAllDelegatesPaginated(
+      pageSize,
+      page,
+    );
+
+    if (localDelegatesBatch.length < pageSize) {
+      hasMore = false; // This was the last page
+    }
+
+    for (const localDelegate of localDelegatesBatch) {
+      const l1Delegate = delegatesSnapshotL1.find(
+        (d) => d.id.toLowerCase() === localDelegate.ethAddress?.toLowerCase(),
+      );
+      const l2Delegate = delegatesSnapshotL2.find(
+        (d) =>
+          d.id.toLowerCase() === localDelegate.starknetAddress?.toLowerCase(),
+      );
+
+      const votingPowerL1 = l1Delegate
+        ? parseInt(l1Delegate.delegatedVotes)
+        : 0;
+      const votingPowerL2 = l2Delegate
+        ? parseInt(l2Delegate.delegatedVotes)
+        : 0;
+
+      // Combine the logic for saving/updating the delegate votes here
+      ///console.log({...localDelegate, votingPowerL1, votingPowerL2});
+      await saveDelegateVotes({
+        delegateId: localDelegate.id,
+        address: localDelegate.address,
+        votingPowerLayerOne: votingPowerL1,
+        votingPowerLayerTwo: votingPowerL2,
+        votingPower: votingPowerL1 + votingPowerL2,
+      });
+    }
+
+    page++; // Go to the next page
+  }
 }
 
 delegateVoting();
 
-export default delegateVoting
+export default delegateVoting;
