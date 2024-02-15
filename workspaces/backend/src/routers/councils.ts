f73af83c9f7985b6621e5883f2fdc1fdff8ac94f
xgrizx <xxxgrizxxx@gmail.com>
2024-02-15 09:13:50 +0100

    refactored routes except socials

--- workspaces/backend/src/routers/councils.ts
+++ workspaces/backend/src/routers/councils.ts
@@ -1,4 +1,4 @@
-import { router, publicProcedure, protectedProcedure } from '../utils/trpc';
+import {router, publicProcedure, protectedProcedure, isAdmin} from '../utils/trpc';
 import { councils } from '../db/schema/councils';
 import { db } from '../db/db';
 import { and, desc, eq } from 'drizzle-orm';
@@ -161,7 +161,7 @@ export const councilsRouter = router({
       return updatedCouncil[0];
     }),
 
-  deleteCouncil: publicProcedure
+  deleteCouncil: protectedProcedure
     .input(
       councilInsertSchema
         .required({ id: true, slug: true })
@@ -208,6 +208,7 @@ export const councilsRouter = router({
 
   deleteUserFromCouncil: protectedProcedure
     .input(z.object({ id: z.number(), councilId: z.number() }))
+    .use(isAdmin)
     .mutation(async (opts) => {
       const user = await db.query.members.findFirst({
         where: eq(members.id, opts.input.id),
