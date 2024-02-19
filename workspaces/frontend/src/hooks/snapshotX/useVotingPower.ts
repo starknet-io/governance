46e99490a9a1c3ee002efc91d20c8c9901428fb5
xgrizx <xxxgrizxxx@gmail.com>
2024-02-19 21:35:49 +0100

    fix voting power initial

--- workspaces/frontend/src/hooks/snapshotX/useVotingPower.ts
+++ workspaces/frontend/src/hooks/snapshotX/useVotingPower.ts
@@ -26,7 +26,7 @@ export function useVotingPower({
 
   const fetchVotingPower = async () => {
     if (!address || !spaceObj?.space || !address.length) {
-      setData(0n);
+      setData(0);
       setIsLoading(false);
       return;
     }
