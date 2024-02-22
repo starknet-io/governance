eed0f155b93d0f7d9a4492920feb99986f9534bc
xgrizx <xxxgrizxxx@gmail.com>
2024-02-22 13:51:13 +0100

    return certificate to snapshot

--- workspaces/backend/src/db/db.ts
+++ workspaces/backend/src/db/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import * as fs from 'fs';
 import dotenv from 'dotenv';
 
 import * as comments from './schema/comments';
@@ -28,6 +29,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    ca: fs.readFileSync('ca-certificate.crt').toString(),
+  },
 });
 
 const db = drizzle(pool, {
